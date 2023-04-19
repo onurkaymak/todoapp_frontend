25e2b4b6806caf495022e18fdce72cf145f3e686
onurkaymak <onurkaymak@outlook.com>
2023-04-18 22:49:16 -0700

    styling changes on NewTodo component

--- src/components/todos/NewTodo.js
+++ src/components/todos/NewTodo.js
@@ -1,7 +1,7 @@
 import classes from './NewTodo.module.scss';
 import Card from '../../UI/Card';
 
-import { Fragment, useRef } from 'react';
+import { Fragment, useRef, useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -11,20 +11,23 @@ import { createTodo } from '../../store/todo-actions';
 
 import Todos from './Todos';
 
+import { FlagIcon } from '../../img/icons/FlagIcon';
+
 
 const NewTodo = () => {
 
     const isInputEmpty = useSelector(state => state.todos.isInputEmpty);
-
-
     const token = useSelector(state => state.user.token);
 
     const dispatch = useDispatch();
     const inputRef = useRef();
     const importantRef = useRef();
 
+    const [isFlagOn, setIsFlagOn] = useState(false);
 
-
+    const flagHandler = () => {
+        setIsFlagOn(() => !isFlagOn)
+    }
 
     const todoSubmitHandler = async (event) => {
         event.preventDefault();
@@ -41,28 +44,41 @@ const NewTodo = () => {
         }
 
         dispatch(createTodo(userInput));
+        setIsFlagOn(false)
         inputRef.current.value = '';
         dispatch(todoActions.inputEmpty(false));
     };
 
+
+
+
     return (
         <Fragment>
-            <div>
+            <div className={classes.container__newTodo}>
                 <Card>
                     <form className={classes.form} onSubmit={todoSubmitHandler}>
-                        <input name="new todo" id="new-todo" ref={inputRef} />
-                        <div className={classes.div}>
-                            <input type="checkbox" name='important' id='important' ref={importantRef}></input>
-                            <label>Check as an important todo</label>
+                        <input name="new todo" id="new-todo" ref={inputRef} className={classes.form__input}></input>
+                        <div className={classes.form__icon__container}>
+                            <input type="checkbox" name='important' id='important' ref={importantRef}
+                                className={classes.form__important} onClick={flagHandler}></input>
+                            <FlagIcon flagColor={isFlagOn} />
+                            <button className={classes.form__button_submit}>Add</button>
                         </div>
-                        <button className='btn btn-primary'>Add New Todo!</button>
                     </form>
                 </Card>
                 {isInputEmpty && <p className={classes.p}>You cannot add an empty todo!</p>}
             </div>
-            <Todos />
+            {/* <Todos /> */}
         </Fragment>
     )
 };
 
 export default NewTodo;
+
+
+
+/* <div className={classes.div}>
+    <input type="checkbox" name='important' id='important' ref={importantRef}></input>
+    <label>Check as an important todo</label>
+</div>
+<button className='btn btn-primary'>Add New Todo!</button> */
