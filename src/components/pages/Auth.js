2034b8ced75f4e133436520b0d8a92f796909050
onurkaymak <onurkaymak@outlook.com>
2023-05-16 14:51:17 -0700

    note added

--- src/components/pages/Auth.js
+++ src/components/pages/Auth.js
@@ -9,6 +9,7 @@ const Auth = () => {
         <div className={classes.authContainer}>
             <h1 className={classes.h1}>Welcome to Todo App</h1>
             <AuthForm />
+            <p className={classes.p}> Please note that loading may take longer than expected, the back-end server is built on a free-tier hosting provider due to demo purposes.</p>
         </div>
     )
 };
